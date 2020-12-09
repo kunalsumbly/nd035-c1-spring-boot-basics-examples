6ed5f1300f5b7821d2573162f7a9afdbebefef7f
kunalsumbly <kunal.sumbly@gmail.com>
2020-12-10 01:12:36 +1100

    udacity example 2 commit

--- udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
+++ udacity-jwdnd-c1-l3-mvc-and-you-1-master/src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -1,14 +1,11 @@
 package com.example.mvc_basics.controller;
 
-import java.time.Instant;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.mvc.model.Cat;
 
 @Controller
 public class HomeController {
@@ -21,10 +18,16 @@ public class HomeController {
 	 * "red", 10); model.addAttribute("cat", myCat); return "home"; }
 	 */
     
-    @RequestMapping(value="/simplehome")
+    @RequestMapping(value="/simplehome1")
     public String getGreetings(@RequestParam(value="firstVisit", required = false) String firstVisit,Model model) {
     	model.addAttribute("firstVisit", !"Visit me".equals(firstVisit));
     	return "home";
     }
 
+    @RequestMapping(value="/simplehome")
+    public String simpleHome(Model model, HttpServletRequest request) {
+    	String parameter = request.getParameter("firstVisit");
+    	model.addAttribute("firstVisit", parameter == null);
+    	return "home";
+    }
 }
